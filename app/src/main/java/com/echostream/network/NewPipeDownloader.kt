fd83479dbd9bf44a5b4def70a127b0cde439bd3c
Aeroflash-r1 <roopang2009@gmail.com>
2026-07-01 20:15:50 +0000

    fix: Downloader is still a class in v0.26.3, use parentheses

--- app/src/main/java/com/echostream/network/NewPipeDownloader.kt
+++ app/src/main/java/com/echostream/network/NewPipeDownloader.kt
@@ -6,7 +6,7 @@ import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.downloader.Request
 import org.schabi.newpipe.extractor.downloader.Response
 
-class NewPipeDownloader(private val client: OkHttpClient) : Downloader {
+class NewPipeDownloader(private val client: OkHttpClient) : Downloader() {
 
     override fun execute(request: Request): Response {
         val builder = okhttp3.Request.Builder().url(request.url())
